fbb8a048023bd4ea624665846f35009cf30e6f92
101rockyprojects <rockybarrios10@gmail.com>
2024-05-13 22:14:44 -0500

    init project

---
+++ src/api/refugio/controllers/refugio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * refugio controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::refugio.refugio');
