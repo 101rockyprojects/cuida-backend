6fe547d4f85f83750da574ca0768dd22f5975af6
101rockyprojects <rockybarrios10@gmail.com>
2024-05-28 14:30:57 -0500

    Feat english translations

--- src/admin/app.js
+++ src/admin/app.js
@@ -39,19 +39,51 @@ const config = {
       colors: {
         neutral0: '#202024', // Card
         neutral100: '#2B2B2F',  // Background
-        primary100: '#423E4B',  // Selected
+        primary100: '#312E38',  // Selected
         primary600: '#F18A41', // Text Selected Left Menu
         buttonPrimary500: '#F18A41', // Button
       }
     }
   },
   translations: {
+    en: {
+      "app.components.LeftMenu.navbrand.title": "Cuida",
+      "global.content-manager": "Content Management",
+      "content-manager.plugin.name": "Content Management",
+      "global.plugins.content-type-builder": "Manage Content",
+      "global.plugins.upload.description": "Multimedia File Management",
+      "global.marketplace": "Plugin Store",
+      "cloud.plugin.name": "Strapi Cloud",
+      "app.components.LeftMenu.navbrand.workplace": "Administration Panel",
+      "content-manager.api.id": "Content Type",
+      "global.select-all-entries": "Select all items",
+      "New entry": "New item",
+      "Settings.apiTokens.create": "Add item",
+      "components.PageFooter.select": "items per page",
+      "content-manager.HeaderLayout.button.label-add-entry": "Create new item",
+      "content-manager.components.TableDelete.label": "{number, plural, one {# selected item} other {# selected items}}",
+      "content-manager.containers.Edit.delete-entry": "Delete this item",
+      "content-manager.containers.Edit.pluginHeader.title.new": "Create an item",
+      "content-manager.containers.EditView.add.new-entry": "Add an item",
+      "content-manager.form.Input.pageEntries": "{ContentType}s per page",
+      "content-manager.pages.ListView.header-subtitle": "{number, plural, =0 {# items found} one {# item found} other {# items found}}",
+      "content-manager.utils.data-loaded": "{number, plural, =1 {Loaded successfully} other {Loaded successfully}}",
+      "content-manager.header.name": "My content",
+      "content-manager.components.LeftMenu.collection-types": "My information",
+      "app.components.EmptyStateLayout.content-document": "No items found yet. Take the first step!",
+      "Auth.form.welcome.title": "Welcome to Cuida!",
+      "Auth.form.welcome.subtitle": "Log in and start managing your shelter.",
+      "app.components.ToggleCheckbox.off-label": "False",
+      "app.components.ToggleCheckbox.on-label": "True",
+      "components.Input.error.validation.regex": "This value does not have a valid format.",
+      "content-manager.error.validation.regex": "The value does not have a valid format."
+    },
     es: {
-      'app.components.LeftMenu.navbrand.title': 'Cuida',
+      "app.components.LeftMenu.navbrand.title": "Cuida",
       "global.content-manager": "Administrar contenido",
       "content-manager.plugin.name": "Administrar contenido",
       "global.plugins.content-type-builder": "Gestionar contenido",
-      "global.plugins.upload.description": "Gestión de archivos multimedia.",
+      "global.plugins.upload.description": "Gestión de archivos multimedia",
       "global.marketplace": "Tienda de plugins",
       "cloud.plugin.name": "Strapi Cloud",
       "app.components.LeftMenu.navbrand.workplace": "Panel de administración",
@@ -74,7 +106,9 @@ const config = {
       "Auth.form.welcome.title": "¡Bienvenido a Cuida!",
       "Auth.form.welcome.subtitle": "Inicia sesión y empieza a administrar tu refugio.",
       "app.components.ToggleCheckbox.off-label": "Falso",
-      "app.components.ToggleCheckbox.on-label": "Verdadero"
+      "app.components.ToggleCheckbox.on-label": "Verdadero",
+      "components.Input.error.validation.regex": "Este valor no tiene un formato válido.",
+      "content-manager.error.validation.regex": "El valor no tiene un formato válido.",
     },
   },
 };
