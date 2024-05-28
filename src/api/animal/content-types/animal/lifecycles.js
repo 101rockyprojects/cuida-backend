745a01cb2c4c38e1d934224faaddd11091107406
101rockyprojects <rockybarrios10@gmail.com>
2024-05-27 23:24:52 -0500

    Fix error when publish an animal

--- src/api/animal/content-types/animal/lifecycles.js
+++ src/api/animal/content-types/animal/lifecycles.js
@@ -7,11 +7,13 @@ module.exports = {
     async beforeCreate(event) {
       const {data} = event.params;
       data.slug = await generateSlug("api::animal.animal", data);
-      data.fotos.forEach(foto => {
-        if (!foto.alternativeText) {
-          throw new ValidationError('Por favor agrega un texto alternativo a todas las fotos');
-        }
-      });
+      if (Object.keys(data).length !== 3) {
+        data.fotos.forEach(foto => {
+          if (!foto.alternativeText) {
+            throw new ValidationError('Por favor agrega un texto alternativo a todas las fotos');
+          }
+        });
+      }
       const userId = data.createdBy;
       const refugioId = await getRefugioByUser(userId);
       const hasPermit = isAdmin();
@@ -22,11 +24,13 @@ module.exports = {
     },
     async beforeUpdate(event) {
         const { data, where } = event.params;
-        data.fotos.forEach(foto => {
-          if (!foto.alternativeText) {
-            throw new ValidationError('Por favor agrega un texto alternativo a todas las fotos');
-          }
-        });
+        if (Object.keys(data).length !== 3) {
+          data.fotos.forEach(foto => {
+            if (!foto.alternativeText) {
+              throw new ValidationError('Por favor agrega un texto alternativo a todas las fotos');
+            }
+          });
+        }
         const id = where.id;
         const existingData = await strapi.entityService.findOne("api::animal.animal", id);
         if (existingData.nombre !== data.nombre) {
